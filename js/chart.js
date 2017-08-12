be1425e51942582d1a8cf851f79bbddead7a5fc3
AhmadHakroosh <ahmad@ahmad-h.com>
2017-08-12 21:32:37 +0300

    added chart generation functions for info popups, chords, and style effects

--- js/chart.js
+++ js/chart.js
@@ -99,4 +99,200 @@
 			return 0.21 * rgb.r + 0.71 * rgb.g + 0.07 * rgb.b;
 		}
     }
+    
+	// arc path generator
+	let textPathArc = d3.svg.arc()
+		.innerRadius(config.outerRadius + 10)
+		.outerRadius(config.outerRadius + 10);
+
+	let textPathArc2 = d3.svg.arc()
+		.innerRadius(config.outerRadius + 18)
+		.outerRadius(config.outerRadius + 18);
+
+	// arc generator
+	let arc = d3.svg.arc()
+		.innerRadius(config.innerRadius)
+		.outerRadius(config.outerRadius);
+
+	// chord diagram
+	let layout = Globalmigration.layout()
+		.padding(config.arcPadding)
+		.threshold(config.layout.threshold)
+		.data(data)
+		.year(config.now);
+
+	if (config.layout.sortGroups) {
+		layout.sortGroups(config.layout.sortGroups);
+	}
+
+	if (config.layout.sortSubgroups) {
+		layout.sortSubgroups(config.layout.sortSubgroups);
+	}
+
+	if (config.layout.sortChords) {
+		layout.sortChords(config.layout.sortChords);
+	}
+
+	if (config.layout.alpha) {
+		layout.alpha(config.layout.alpha);
+	}
+
+	// chord path generator
+	let chordGenerator = Globalmigration.chord()
+		.radius(config.innerRadius)
+		.sourcePadding(config.sourcePadding)
+		.targetPadding(config.targetPadding);
+
+	// svg element
+	let svg = d3.select(config.element).append("svg")
+		.attr('preserveAspectRatio', 'xMidYMid')
+		.attr('viewBox', '0 0 ' + config.width + ' ' + config.height)
+		.attr("width", config.width)
+		.attr("height", config.height);
+
+	let element = svg.append("g")
+		.attr("id", "circle")
+		.attr("transform", "translate(" + config.width / 2 + "," + config.height / 2 + ")");
+
+	d3.select(window).on('resize.svg-resize', () => {
+		let width = svg.node().parentNode.clientWidth;
+
+		if (width) {
+			// make height adapt to shrinking of page
+			if (width < config.width) {
+				svg.attr('height', width);
+			}
+		}
+	});
+
+	// needed for fade mouseover
+	let circle = element.append("circle").attr("r", config.outerRadius + 24);
+	let filter = svg.append('filter').attr('id', 'dropshadow');
+
+	filter.append('feGaussianBlur').attr({
+		in: 'SourceAlpha',
+		stdDeviation: 2
+	});
+
+	filter.append('feOffset').attr({
+		dx: 0,
+		dy: 1,
+		result: 'offsetblur'
+	});
+
+	filter.append('feComponentTransfer').append('feFuncA').attr({
+		type: 'linear',
+		slope: 0.5
+	});
+
+	let femerge = filter.append('feMerge');
+
+	femerge.append('feMergeNode');
+	femerge.append('feMergeNode').attr('in', 'SourceGraphic');
+
+	let info = svg.append('g')
+		.attr('class', 'info-group')
+		.attr("transform", "translate(" + config.width / 2 + "," + config.height / 2 + ")")
+		.append('g')
+		.attr('class', 'info')
+		.attr('opacity', 0);
+
+	info.append('rect').style('filter', 'url(#dropshadow)');
+
+	info.append('g').attr('class', 'text');
+
+	svg.on('mousemove', () => {
+		info.transition().duration(10).attr('opacity', 0);
+	});
+
+	circle.on('mouseout', () => {
+		if (infoTimer) {
+			clearTimeout(infoTimer);
+		}
+		info.transition().duration(10).attr('opacity', 0);
+	});
+
+	let infoTimer;
+
+	// eg: West Africa: Total inflow 46, Total outflow 2
+	function groupInfo (d) {
+		let el = this;
+
+		if (infoTimer) {
+			clearTimeout(infoTimer);
+		}
+
+		let bbox = el.getBBox();
+		infoTimer = setTimeout(() => {
+			let color = d3.select(el).style('fill');
+
+			info.attr('transform', 'translate(' + (bbox.x + bbox.width / 2) + ',' + (bbox.y + bbox.height / 2) + ')');
+
+			let text = info.select('.text').selectAll('text')
+				.data([
+					data.names[d.id],
+					'Total In: ' + formatNumber(d.inflow),
+					'Total Out: ' + formatNumber(d.outflow)
+				]);
+
+			text.enter().append('text');
+			text.text(function(t) { return t; }).style({
+				fill: luminicity(color) > 160 ? 'black' : 'white'
+			}).attr({
+				transform: (t, i) => {
+					return 'translate(6, ' + (i * 14 + 16) + ')';
+				}
+			});
+
+			text.exit().remove();
+			let tbbox = info.select('.text').node().getBBox();
+
+			info.select('rect').style('fill', color).attr({
+				width: tbbox.width + 12,
+				height: tbbox.height + 10
+			});
+
+			info.transition().attr('opacity', 1);
+		}, config.infoPopupDelay);
+	}
+
+	// chord info
+	// eg: West Asia → Pacific: 46
+	function chordInfo (d) {
+		let el = this;
+
+		if (infoTimer) {
+			clearTimeout(infoTimer);
+		}
+
+		let bbox = el.getBBox();
+		infoTimer = setTimeout(() => {
+			let color = d3.select(el).style('fill');
+
+			info.attr('transform', 'translate(' + (bbox.x + bbox.width / 2) + ',' + (bbox.y + bbox.height / 2) + ')')
+				.attr('opacity', 0)
+				.transition()
+				.attr('opacity', 1);
+
+			let text = info.select('.text').selectAll('text').data([
+				data.names[d.source.id] + ' → ' + data.names[d.target.id] + ': ' + formatNumber(d.source.value)
+			]);
+
+			text.enter().append('text');
+			text.exit().remove();
+			text.text((t) => { return t; }).style({
+				fill: luminicity(color) > 160 ? 'black' : 'white'
+			}).attr('transform', (t, i) => {
+				return 'translate(6, ' + (i * 12 + 16) + ')';
+			});
+
+			info.selectAll('rect').style('fill', d3.select(el).style('fill'));
+
+			let tbbox = info.select('.text').node().getBBox();
+			info.select('rect').attr({
+				width: tbbox.width + 12,
+				height: tbbox.height + 10
+			});
+		}, config.infoPopupDelay);
+	}
 });
