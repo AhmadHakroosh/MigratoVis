38dc48592efd98c3300a3472c83b9279bc9fab5b
AhmadHakroosh <ahmad@ahmad-h.com>
2017-08-12 22:38:57 +0300

    created chord.js - and extension for d3 standard chord but with sourcePadding and targetPadding

---
+++ js/chord.js
@@ -0,0 +1,10 @@
+// This is an extension for d3.svg.chord
+// contains sourcePadding and targetPadding
+
+((scope) => {
+
+	scope.chord = () => {
+		// TODO
+	};
+
+})(window.migrato || (window.migrato = {});
