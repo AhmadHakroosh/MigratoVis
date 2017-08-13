99d8c07c77312afb0ac606a0a4f5f7613b7e5c69
AhmadHakroosh <ahmad@ahmad-h.com>
2017-08-13 12:24:57 +0300

    basic periods functionality by form

--- js/periods.js
+++ js/periods.js
@@ -1,1 +1,17 @@
 // Period selection functionality implementation
+
+((scope) => {
+	scope.periods = (chart, config) => {
+		// Find and set years
+		let years = Object.keys(chart.data.mapping).map((x) => { return parseInt(x); });
+		// Initialize configuration
+		config = config || {};
+		config.element = config.element || 'body';
+		config.now = config.now || years[0];
+		config.incr = config.incr || 5;
+
+		let form = d3.select(config.element).append('form');
+		let year = form.selectAll('.year').data(years);
+		let span = year.enter().append('span').classed('year', true);
+	};
+})(window.migrato || (window.migrato = {}));
