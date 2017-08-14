3bd73b1f26d2b2cb48b3469d98535498cc1219cb
AhmadHakroosh <ahmad@ahmad-h.com>
2017-08-14 22:44:19 +0300

    set initial variables; getting region by country index

--- js/layout.js
+++ js/layout.js
@@ -1,6 +1,33 @@
 // The application layout configuration
 
 (function (scope) {
-	
-});
+	scope.layout = function() {
+		var chord = {},
+			chords,
+			groups,
+			data,
+			matrix,
+			indices,
+			countries,
+			year,
+			n,
+			padding = 0,
+			threshold = null,
+			sortGroups,
+			sortSubgroups,
+			sortChords,
+			alpha;
+
+		function region (index) {
+			var r = 0;
+			for (var i = 0; i < data.regions.length; i++) {
+				if (data.regions[i] > index) {
+					break;
+				}
+				r = i;
+			}
+			return data.regions[r];
+		}
+	};	
+})(window.migrato || (window.migrato = {}));
 
